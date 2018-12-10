17aad07d91adb8eaec65ffd27a881e9f86f4cccd
howjinwong <howjinwong@gmail.com>
2018-12-10 15:40:53 +0000

    Updating Maze algorithm code

---
+++ Maze Algorithm/Maze_Algorithm.c
@@ -0,0 +1,14 @@
+//Maze Algorithm
+
+//Initialise the all the cells, give all cells a value from 0 to 
+
+/* 
+0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0 
+*/
